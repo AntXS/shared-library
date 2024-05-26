41de41c4412b839151996b4256cc8d00222f6dfb
Github API Testing <71627473+AntXS@users.noreply.github.com>
2024-05-26 13:37:58 +0530

    Add files via upload

---
+++ src/org/example/utilities/Utility.groovy
@@ -0,0 +1,8 @@
+// src/org/example/utilities/Utility.groovy
+package org.example.utilities
+
+class Utility {
+    static void printMessage(String message) {
+        println "Utility message: ${message}"
+    }
+}
